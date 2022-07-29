7168cbf8cd5f75fcaf50cd3aa0cc4f68a40924c5
Pierre Willenbrock <pierre.willenbrock@dfki.de>
2022-07-29 13:09:56 +0200

    Flavor qt5

--- init.rb
+++ init.rb
@@ -45,12 +45,16 @@ Rock.flavors.define 'master', :implicit => true
 
 configuration_option('ROCK_SELECTED_FLAVOR', 'string',
     :default => 'master',
-    :possible_values => ['stable', 'master'],
+    :possible_values => ['stable', 'master', 'qt5'],
     :doc => [
         "Which flavor of Rock do you want to use ?",
-        "Stay with the default ('master') if you want to use Rock on the most recent",
-        "distributions (Ubuntu 16.04 and later). Use 'stable' only for ",
-        "now officially unsupported distributions (Ubuntu 14.04)"])
+        "Stay with the default ('master') if you want to use Rock on",
+        "distributions where qt4 support exists (Ubuntu 16.04 to 20.04).",
+        "Use 'qt5' on newer distributions (Ubuntu 21.04 and later) or",
+        "if you want to use qt5 on your qt5 supporting distribution",
+        "(Ubuntu 18.04 and later). The 'qt5' flavor is not as complete as",
+        "the 'master' flavor. Use 'stable' only for",
+        "long unsupported distributions (Ubuntu 14.04)"])
 
 if Rock.in_release? && !Autoproj.config.has_value_for?('ROCK_SELECTED_FLAVOR')
     Autoproj.config.set 'ROCK_SELECTED_FLAVOR', 'stable', true
