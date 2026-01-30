fa735fe00f0707a34a2bd85640b8f38b0411560b
Steffen Planthaber <steffen.planthaber@dfki.de>
2026-01-30 10:59:08 +0100

    fix call again

--- rock/python.rb
+++ rock/python.rb
@@ -404,7 +404,7 @@ module Rock
             ws.install_os_packages(["python-pip"])
             python_executable = get_python_from_config.first
             puts "Upgrading pip in venv"
-            Autobuild::Subprocess.run "config", "upgrade_pip", "/bin/bash", "-c", ". ./install/venv/bin/activate; python3 -m pip install --upgrade pip"md
+            Autobuild::Subprocess.run "config", "upgrade_pip", "/bin/bash", "-c", ". ./install/venv/bin/activate; python3 -m pip install --upgrade pip"
         end
 
         def self.upgrade_pip(ws: Autoproj.workspace)
